9f7803de320af788fde69615a537a941896c661d
Cian Hughes <chughes000@gmail.com>
2025-06-10 10:25:12 +0100

    Added parametrically configurable container env

--- devenv.nix
+++ devenv.nix
@@ -4,10 +4,21 @@
   config,
   inputs,
   ...
-}: {
+}: let
+  containerPkg = let
+    containerEnv = builtins.getEnv "CONTAINER_ENV";
+  in
+    if containerEnv == "docker" || containerEnv == ""
+    then pkgs.docker
+    else if containerEnv == "podman"
+    then pkgs.podman
+    else if containerEnv == "kubernetes"
+    then pkgs.minikube
+    else throw "Invalid env variable `CONTAINER_ENV` provided!";
+in {
   packages = [
+    containerPkg
     pkgs.act
-    pkgs.docker
     pkgs.git
   ];
 }
