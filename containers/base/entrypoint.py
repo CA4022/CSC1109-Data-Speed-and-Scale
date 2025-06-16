83746dfe5a42903544516905df90b24f14f44b44
Cian Hughes <chughes000@gmail.com>
2025-06-16 14:59:30 +0100

    Consolidated entrypoint into python file

---
+++ containers/base/entrypoint.py
@@ -0,0 +1,207 @@
+#!/usr/bin/env python3.12
+
+import asyncio
+import os
+import subprocess
+
+
+SPLASH = """\033[35m
+     .o88b. .d8888.  .o88b.  db  db  .d88b.  .d888b.
+    d8P  Y8 88'  YP d8P  Y8 o88 o88 .8P  88. 88' `8D
+    8P      `8bo.   8P       88  88 88  d'88 `V8o88'
+    8b        `Y8b. 8b       88  88 88 d' 88    d8'
+    Y8b  d8 db   8D Y8b  d8  88  88 `88  d8'   d8'
+     `Y88P' `8888Y'  `Y88P'  VP  VP  `Y88P'   d8'\033[0m"""
+
+
+def display_markdown(files: list[str]):
+    with subprocess.Popen(["glow"], stdin=subprocess.PIPE) as glow_proc:
+        assert glow_proc.stdin is not None
+        for fname in files:
+            with open(fname, "rb") as f:
+                glow_proc.stdin.write(f.read())
+        glow_proc.stdin.close()
+        glow_proc.wait()
+
+
+async def start_daemon(
+    command: str,
+    check_command: str,
+    log_file: str = "/dev/null",
+    poll_interval: int = 1,
+    timeout: int = 60,
+) -> None:
+    with open(log_file, "wb") as log:
+        process = await asyncio.create_subprocess_shell(command, stdout=log, stderr=log)
+
+    async def poll_daemon():
+        while True:
+            check_process = await asyncio.create_subprocess_shell(
+                check_command,
+                stdout=asyncio.subprocess.DEVNULL,
+                stderr=asyncio.subprocess.DEVNULL,
+            )
+            await check_process.wait()
+
+            if check_process.returncode == 0:
+                break
+
+            if process.returncode is not None:
+                msg = f"Daemon '{command}' exited unexpectedly with code {process.returncode}."
+                msg = f"{msg} Check logs at {log_file}."
+                raise RuntimeError(msg)
+
+            print(f"Waiting for {command}...")
+            await asyncio.sleep(poll_interval)
+
+    try:
+        await asyncio.wait_for(poll_daemon(), timeout=timeout)
+    except asyncio.TimeoutError as e:
+        print(f"❌ Timed out after {timeout} seconds waiting for {command}.")
+        if process.returncode is None:
+            process.terminate()
+            await process.wait()
+        raise e
+
+
+class Selector:
+    def __init__(self, name: str, prompt: str, items: list[tuple[str, str, str]]):
+        self.name = name
+        self.prompt = prompt
+        self.options = tuple(i[0] for i in items)
+        self.icons = {i[0]: i[1] for i in items}
+        self.commands = {i[0]: i[2] for i in items}
+
+    async def select(self):
+        # Enforce sanity checks to prevent configuration problems
+        assert hasattr(self, "options")
+        assert hasattr(self, "icons")
+        assert hasattr(self, "commands")
+        for i in self.options:
+            assert i in self.icons
+            assert i in self.commands
+
+        while True:
+            print(self.prompt)
+            for i, opt in enumerate(self.options):
+                print(f"{i}) {opt} {self.icons[opt]}")
+            choice = input("Enter a choice [default=0]: ")
+            try:
+                choice = choice if choice else 0
+                choice = choice if choice in self.options else self.options[int(choice)]
+                break
+            except ValueError:
+                print()
+                print(f"{choice} is not a valid choice! Please try again.")
+            except IndexError:
+                print()
+                print(f"Option {choice} does not exist! Please try again.")
+            print()
+        self.choice = choice
+
+    def print_choice(self, indent: int = 0):
+        print(f"{' ' * indent * 4}{self.name}: {self.choice} {self.icons[self.choice]}")
+
+    def start(self):
+        raise NotImplementedError(
+            "The method `start` has not been implemented for this selector."
+        )
+
+
+class EnvSelector(Selector):
+    def __init__(
+        self, name: str, prompt: str, items: list[tuple[str, str, str]], env_var: str
+    ):
+        super().__init__(name, prompt, items)
+        self.env_var = env_var
+
+    def start(self):
+        os.environ[self.env_var] = self.choice
+
+
+class ShellSelector(Selector):
+    def __init__(
+        self,
+        name: str,
+        prompt: str,
+        items: list[tuple[str, str, str]],
+        selectors: list[Selector] | None = None,
+    ):
+        super().__init__(name, prompt, items)
+        self.selectors = selectors if selectors is not None else []
+
+    async def select(self):
+        print()
+        await super().select()
+        print()
+        for s in self.selectors:
+            await s.select()
+            print()
+
+    def print_choice(self, indent: int = 0):
+        super().print_choice(indent=indent)
+        for s in self.selectors:
+            s.print_choice(indent=indent)
+
+    def start(self):
+        assert hasattr(self, "choice")
+        print("Launching shell with:")
+        self.print_choice(indent=1)
+        print()
+        for s in self.selectors:
+            s.start()
+        try:
+            os.execvp(self.choice, [self.choice])
+        except FileNotFoundError as e:
+            msg = (
+                f"Error: The shell '{self.choice}' could not be found on your system.",
+            )
+            raise EnvironmentError(msg) from e
+
+
+async def main():
+    daemon_startup = asyncio.create_task(
+        start_daemon("dockerd", "docker info", "/var/log/dockerd.log")
+    )
+
+    selector = ShellSelector(
+        "Shell",
+        "\x1b[32m\033[0m Please choose your preferred shell environment:",
+        [
+            ("fish", "\x1b[31m󰈺\033[0m", "fish"),
+            ("bash", "", "bash"),
+            ("zshell", "", "zsh"),
+            ("nushell", "\x1b[32m❯\033[0m", "nu"),
+        ],
+        [
+            EnvSelector(
+                "Editor",
+                "\033[90m\033[0m Please select your default text editor:",
+                [
+                    ("micro", "\x1b[38;5;97mμ\033[0m", "micro"),
+                    ("vim", "\x1b[38;5;2m\033[0m", "vim"),
+                    ("neovim", "\x1b[38;5;12m\033[0m", "nvim"),
+                    ("emacs", "\x1b[38;5;5m\033[0m", "emacs"),
+                ],
+                "EDITOR",
+            )
+        ],
+    )
+
+    try:
+        await selector.select()
+        print(SPLASH)
+        display_markdown(["/root/CSS1109.md", "/lab/lab.md"])
+        await daemon_startup
+        selector.start()
+
+    except (RuntimeError, asyncio.TimeoutError, EnvironmentError) as e:
+        if not daemon_startup.done():
+            daemon_startup.cancel()
+        raise e
+    except asyncio.CancelledError as e:
+        raise e
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
--- containers/base/root/.bashrc
+++ containers/base/root/.bashrc
@@ -11,38 +11,11 @@ eval "$(fzf --bash)"
 export OSH="/root/.oh-my-bash"
 OMB_USE_SUDO=true
 
-completions=(git docker docker-compose ssh)
+completions=(git docker docker-compose maven ssh)
 aliases=(general docker ls)
 plugins=(colored-man-pages fzf git)
 
-if ! command -v maven >/dev/null 2>&1; then
-    completions+=(maven)
-fi
-
 source "$OSH"/oh-my-bash.sh
-
-declare -A edmap
-edmap[micro μ]="micro"
-edmap[vim ]="vim"
-edmap[neovim ]="nvim"
-edmap[emacs ]="emacs"
-
-# Check if EDITOR is already set to a valid choice
-if ! [[ "$EDITOR" =~ ^(nvim|vim|emacs|micro)$ ]]; then
-    echo ""
-    echo -e "\033[90m\033[0m Please select your default text editor:"
-    echo "(we recommend micro for those new to working in a CLI)"
-    options=("micro μ" "vim " "neovim " "emacs ")
-    PS3="Enter a number: "
-
-    select opt in "${options[@]}"; do
-        export EDITOR="${edmap[$opt]}"
-        echo ""
-        echo -e "\033[34m\033[0m Default editor set to '$EDITOR'."
-        break
-    done
-    unset PS3
-fi
 
 # Set terminal env variables
 export COLORTERM="truecolor"
@@ -54,16 +27,6 @@ alias cat="bat"
 alias ls="lsd"
 alias grep="rg"
 alias find="fd"
-
-# Finally, let's print some general and lesson specific orientation text.
-echo -e "
-\033[35m .o88b. .d8888.  .o88b.  db  db  .d88b.  .d888b.
-d8P  Y8 88'  YP d8P  Y8 o88 o88 .8P  88. 88' \`8D
-8P      \`8bo.   8P       88  88 88  d'88 \`V8o88'
-8b        \`Y8b. 8b       88  88 88 d' 88    d8'
-Y8b  d8 db   8D Y8b  d8  88  88 \`88  d8'   d8'
- \`Y88P' \`8888Y'  \`Y88P'  VP  VP  \`Y88P'   d8'\033[0m"
-cat /root/CSS1109.md /lab/lab.md | glow
 
 [[ ! ${BLE_VERSION-} ]] || ble-attach
 eval "$(starship init bash)"
