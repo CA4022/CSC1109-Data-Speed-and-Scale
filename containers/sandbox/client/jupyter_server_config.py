fff10dedca12503da0a249b68d74c4ee9bb0c510
Cian Hughes <chughes000@gmail.com>
2025-10-10 11:22:35 +0100

    First attempt to parse off lab6 into sandbox image

---
+++ containers/sandbox/client/jupyter_server_config.py
@@ -0,0 +1,27 @@
+import shutil
+
+
+c = get_config()  # noqa
+c.ServerApp.ip = "0.0.0.0"
+c.IdentityProvider.token = ""
+c.ServerApp.password = ""
+c.ServerApp.allow_unauthenticated_access = True
+c.ServerApp.open_browser = False
+c.ContentsManager.allow_hidden = True
+
+c.LanguageServerManager.language_servers = {
+    "pyright": {
+        "argv": [shutil.which("pyright-langserver"), "--stdio"],
+        "languages": ["python"],
+        "version": 2,
+        "mime_types": ["text/x-python"],
+        "display_name": "Python Language Server (Pyright)",
+    },
+    # "metals": {
+    #     "argv": [shutil.which("metals")],
+    #     "languages": ["scala", "java", "java-source"],
+    #     "version": 2,
+    #     "mime_types": ["text/x-scala", "text/x-java", "text/x-java-source"],
+    #     "display_name": "Scala/Java Language Server (Metals)",
+    # },
+}
